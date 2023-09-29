92caae1cac077faf9928432de6e51a658cdfac49
LilGamerPlayz <thelittlebotengineer@gmail.com>
2023-09-28 23:02:28 -0700

    Update 27
    Finished calendar page
    Built react app

--- src/pages/subpages/calendar.tsx
+++ src/pages/subpages/calendar.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import Navbar from "../../components/ts/navbar/navbar";
 import PageTitle from "../../components/ts/pagetitle/pagetitle";
 import Footer from "../../components/ts/footer/Footer";
+import "../../components/scss/pages/subpages/calendar.scss";
 
 const Calendar = () => {
     return (
         <div id="Calendar">
             <Navbar />
             <PageTitle height={80} mainText="Calendar" subText="See what's happening at Oxford!" oxfLogo={false} backgroundLink="OxfFrontImage.jpg" backgroundVhPreDown={700} backgroundVhDownRate={32} titleVhDownRate={30} percentageDown={32} titleVhPreDown={0} />
+            <h1 id="CalendarMain">School / District Calendars</h1>
             <div id="CalendarContent">
-                <div className="container">
-                </div>
+                    <img id="ImportantDates" src="images/ImportantDatesCalendar.png" />
+                    <img id="DistrictCalendar" src="images/DistrictCalendar.png" />
             </div>
             <Footer title="Calendar" />
         </div>
